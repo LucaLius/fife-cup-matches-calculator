a292a100fbfd055577e9d3a24ec84b3b47911734
LucaLius <luca-lius@hotmail.it>
2024-10-21 11:23:16 +0200

    typescript setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,12 @@
-import jest from "eslint-plugin-jest";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
-export default [...compat.extends("plugin:jest/recommended"), {
-    plugins: {
-        jest,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...jest.environments.globals.globals,
-        },
-    },
-}];
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
