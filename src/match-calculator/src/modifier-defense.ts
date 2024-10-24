bcbc9c20e0ba83277cb1d0031d0b92268a01f58a
LucaLius <luca-lius@hotmail.it>
2024-10-24 07:55:00 +0200

    modifier defense

---
+++ src/match-calculator/src/modifier-defense.ts
@@ -0,0 +1,58 @@
+import { ModifierI } from "../../models/modifier.model";
+import { PlayerInfoAVoto } from "../../models/player-info.model";
+import { FormationAnalyzer } from "./formation-analyzer";
+
+export class ModifierDefense implements ModifierI {
+
+  id = 'defense';
+  interestedPlayers: PlayerInfoAVoto[];
+  interestedRoles = ['P', 'D'] as ('P' | 'D' | 'C' | 'A')[];
+
+  private readonly MINUMUM_DEFENDERS_ON_FIELD = 4;
+
+  private readonly SEGMENTS = [
+    { low: 0, high: 6, points: 0 },
+    { low: 6, high: 6.25, points: 1 },
+    { low: 6.25, high: 6.5, points: 2 },
+    { low: 6.5, high: 7, points: 3 },
+    { low: 7, high: 7.25, points: 4 },
+    { low: 7.25, high: 7.5, points: 5 },
+    { low: 7.5, high: 7.75, points: 6 }
+  ];
+
+  constructor(allElevenPlayers: PlayerInfoAVoto[]) {
+    this.interestedPlayers = allElevenPlayers.filter(el => this.interestedRoles.includes(el.role));
+  }
+
+  public calculate(formationAnalyzer: FormationAnalyzer): number | null {
+    const goalKeeper = this.interestedPlayers.find(player => player.role === 'P');
+    const targetDefenders = this.interestedPlayers.filter(player => player.role === 'D');
+    if (formationAnalyzer.numberOfD < this.MINUMUM_DEFENDERS_ON_FIELD) {
+      // non scatta se ho meno difensori di una certa soglia
+      return null;
+    }
+
+    if (!goalKeeper || goalKeeper?.isRiservaUfficio || targetDefenders.some(player => player.isRiservaUfficio)) {
+      // non scatta se uno dei giocatori da considerare è riserva d'ufficio
+      return null;
+    }
+
+    targetDefenders.sort((a, b) => b.fantasyVote - a.fantasyVote);
+    const topThreeDefenders = targetDefenders.slice(0, 3);
+    const targetPlayers = [goalKeeper, ...topThreeDefenders]
+    const mediumValue = targetPlayers
+      .map(player => player.fantasyVote)
+      .reduce(getSum) / targetPlayers.length;
+
+    const segment = this.SEGMENTS.find(el => mediumValue >= el.low && mediumValue < el.high);
+    if (!segment) {
+      throw new Error("Segment not found!!");
+    }
+
+    return segment.points;
+  };
+}
+
+function getSum(acc: number, value: number): number {
+  return acc + value;
+}
---
+++ src/match-calculator/src/modifiers-defense.test.ts
@@ -0,0 +1,152 @@
+import { FormationAnalyzer } from './formation-analyzer';
+import { ModifierDefense } from './modifier-defense';
+import { describe, expect, test } from '@jest/globals';
+import { TeamInfo } from '../../models/team-info.model';
+import { PlayerInfoAVoto } from '../../models/player-info.model';
+
+/* eslint-env jest */
+
+describe('ModifierDefense.calculate()', () => {
+  test('Should calculate how many points from defense modifier, (not applicable case for riserva d\'ufficio goalkeeper)', () => {
+    const given = {
+      teamInfo: {
+        formation: '4-4-2'
+      } as TeamInfo,
+      allPlayers:
+        [
+          { role: 'P', fantasyVote: 1, isRiservaUfficio: true },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+        ] as PlayerInfoAVoto[]
+    };
+
+    const expected = null;
+
+    const modifierDefense = new ModifierDefense(given.allPlayers);
+    const formationAnalyzer = new FormationAnalyzer(given.teamInfo);
+    const actual = modifierDefense.calculate(formationAnalyzer);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('Should calculate how many points from defense modifier, (not applicable case for riserva d\'ufficio defender)', () => {
+    const given = {
+      teamInfo: {
+        formation: '4-4-2'
+      } as TeamInfo,
+      allPlayers:
+        [
+          { role: 'P', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 3, isRiservaUfficio: true },
+        ] as PlayerInfoAVoto[]
+    };
+
+    const expected = null;
+
+    const modifierDefense = new ModifierDefense(given.allPlayers);
+    const formationAnalyzer = new FormationAnalyzer(given.teamInfo);
+    const actual = modifierDefense.calculate(formationAnalyzer);
+
+    expect(actual).toEqual(expected);
+  })
+
+  test('Should calculate how many points from defense modifier, (not applicable case for not enough defenders)', () => {
+    const given = {
+      teamInfo: {
+        formation: '3-4-3'
+      } as TeamInfo,
+      allPlayers:
+        [
+          { role: 'P', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+        ] as PlayerInfoAVoto[]
+    };
+
+    const expected = null;
+
+    const modifierDefense = new ModifierDefense(given.allPlayers);
+    const formationAnalyzer = new FormationAnalyzer(given.teamInfo);
+    const actual = modifierDefense.calculate(formationAnalyzer);
+
+    expect(actual).toEqual(expected);
+  })
+
+  test('Should calculate how many points from defense modifier, (case easy with bonus == 1)', () => {
+    const given = {
+      teamInfo: {
+        formation: '4-4-2'
+      } as TeamInfo,
+      allPlayers:
+        [
+          { role: 'P', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 6 },
+        ] as PlayerInfoAVoto[]
+    };
+
+    const expected = 1;
+
+    const modifierDefense = new ModifierDefense(given.allPlayers);
+    const formationAnalyzer = new FormationAnalyzer(given.teamInfo);
+    const actual = modifierDefense.calculate(formationAnalyzer);
+
+    expect(actual).toEqual(expected);
+  })
+
+  test('Should calculate how many points from defense modifier, (case medium with bonus == 1 but unordered vote defenders)', () => {
+    const given = {
+      teamInfo: {
+        formation: '4-4-2'
+      } as TeamInfo,
+      allPlayers:
+        [
+          { role: 'P', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 5 },
+          { role: 'D', fantasyVote: 5 },
+          { role: 'D', fantasyVote: 6 },
+          { role: 'D', fantasyVote: 7 },
+        ] as PlayerInfoAVoto[]
+    };
+
+    const expected = 1;
+
+    const modifierDefense = new ModifierDefense(given.allPlayers);
+    const formationAnalyzer = new FormationAnalyzer(given.teamInfo);
+    const actual = modifierDefense.calculate(formationAnalyzer);
+
+    expect(actual).toEqual(expected);
+  })
+
+  test('Should calculate how many points from defense modifier, (case medium with bonus == 3 but unordered vote defenders)', () => {
+    const given = {
+      teamInfo: {
+        formation: '4-4-2'
+      } as TeamInfo,
+      allPlayers:
+        [
+          { role: 'P', fantasyVote: 5 },
+          { role: 'D', fantasyVote: 7 },
+          { role: 'D', fantasyVote: 5 },
+          { role: 'D', fantasyVote: 6.5 },
+          { role: 'D', fantasyVote: 7 },
+        ] as PlayerInfoAVoto[]
+    };
+
+    const expected = 2;
+
+    const modifierDefense = new ModifierDefense(given.allPlayers);
+    const formationAnalyzer = new FormationAnalyzer(given.teamInfo);
+    const actual = modifierDefense.calculate(formationAnalyzer);
+
+    expect(actual).toEqual(expected);
+  })
+})
