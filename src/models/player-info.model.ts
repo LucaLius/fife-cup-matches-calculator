023a0f687802c4240192b158c2db4cede73eb917
LucaLius <luca-lius@hotmail.it>
2024-10-24 07:49:59 +0200

    team info and player info model update

--- src/models/player-info.model.ts
+++ src/models/player-info.model.ts
@@ -1,9 +1,11 @@
 export interface PlayerInfo {
   role: 'P' | 'D' | 'C' | 'A';
   fantasyVote?: number;
+  isRiservaUfficio?: boolean;
 }
 
 export interface PlayerInfoAVoto {
   role: 'P' | 'D' | 'C' | 'A';
   fantasyVote: number;
+  isRiservaUfficio?: boolean;
 }
