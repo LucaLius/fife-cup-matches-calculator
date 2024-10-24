9b59d133e035038478fe0cb19c255cc6eb17d2d3
LucaLius <luca-lius@hotmail.it>
2024-10-24 07:55:09 +0200

    modifiers manager

---
+++ src/match-calculator/src/modifiers-manager.ts
@@ -0,0 +1,27 @@
+import { ModifierI } from "../../models/modifier.model";
+import { PlayerInfoAVoto } from "../../models/player-info.model";
+import { FormationAnalyzer } from "./formation-analyzer";
+import { ModifierDefense } from "./modifier-defense";
+
+export class ModifiersManager {
+
+  public activeModifiers: ModifierI[] = [];
+
+  constructor(allPlayers: PlayerInfoAVoto[]) {
+    this.activeModifiers = [
+      new ModifierDefense(allPlayers)
+    ];
+  }
+
+  public applyModifiers(formationAnalyzer: FormationAnalyzer): number {
+    let totalModifiers = 0;
+    this.activeModifiers.forEach(modifier => {
+      const value = modifier.calculate(formationAnalyzer);
+      if (value !== null) {
+        totalModifiers += value;
+      }
+    });
+
+    return totalModifiers;
+  }
+}
