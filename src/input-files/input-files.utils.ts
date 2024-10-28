9f7108a4d5d12f6e51b9308350308611e5f78852
LucaLius <luca-lius@hotmail.it>
2024-10-28 18:13:36 +0100

    test team file directory

--- src/input-files/input-files.utils.ts
+++ src/input-files/input-files.utils.ts
@@ -1,2 +1,3 @@
 export const INPUT_FILE_DIR_PATH = `${__dirname}`;
 export const INPUT_FILES_TEAMS_DIR_PATH = `${INPUT_FILE_DIR_PATH}/team-files`;
+export const INPUT_TEST_FILES_TEAMS_DIR_PATH = `${INPUT_FILE_DIR_PATH}/test-team-files`;
--- src/teams-info-importer/teams-info-importer.test.ts
+++ src/teams-info-importer/teams-info-importer.test.ts
@@ -2,7 +2,7 @@ import { TeamsInfoImporter } from './teams-info-importer';
 import { describe, expect, test } from '@jest/globals';
 import * as fs from 'fs';
 import { parseXlsx } from "../excel-utils/excel-parser";
-import { INPUT_FILES_TEAMS_DIR_PATH } from '../input-files/input-files.utils';
+import { INPUT_TEST_FILES_TEAMS_DIR_PATH } from '../input-files/input-files.utils';
 
 /* eslint-env jest */
 
@@ -22,10 +22,10 @@ describe('TeamsInfoImporter.getTeamsInfo()', () => {
     // const given = undefined;
     const expected = 4;
 
-    const fileNames = fs.readdirSync(INPUT_FILES_TEAMS_DIR_PATH);
+    const fileNames = fs.readdirSync(INPUT_TEST_FILES_TEAMS_DIR_PATH);
 
     const fileName = fileNames[0]
-    const fileContent: string[][] = parseXlsx(`${INPUT_FILES_TEAMS_DIR_PATH}/${fileName}`);
+    const fileContent: string[][] = parseXlsx(`${INPUT_TEST_FILES_TEAMS_DIR_PATH}/${fileName}`);
 
     const actual = new TeamsInfoImporter().getMatchesInfoIndexes(fileContent);
 
@@ -36,9 +36,9 @@ describe('TeamsInfoImporter.getTeamsInfo()', () => {
 
     const expected = 19;
 
-    const fileNames = fs.readdirSync(INPUT_FILES_TEAMS_DIR_PATH);
+    const fileNames = fs.readdirSync(INPUT_TEST_FILES_TEAMS_DIR_PATH);
     const fileName = fileNames[0];
-    const fileContent: string[][] = parseXlsx(`${INPUT_FILES_TEAMS_DIR_PATH}/${fileName}`);
+    const fileContent: string[][] = parseXlsx(`${INPUT_TEST_FILES_TEAMS_DIR_PATH}/${fileName}`);
 
     const matchesInfoIndexes = new TeamsInfoImporter().getMatchesInfoIndexes(fileContent);
     const matchIndex = 0;
